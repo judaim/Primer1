6e508116c23fd64d147a65fbb82ec3db490926e0
Alex <judai@mail.ru>
2020-06-23 18:17:28 +0300

    Изменение1

--- Primer1/MainWindow.xaml.cs
+++ Primer1/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Primer1
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("sdsdd");
+        }
     }
 }
